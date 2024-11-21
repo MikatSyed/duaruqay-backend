ec48ad41bfdecd3f3833c8241fc3dc2925f6a09b
Syed Gaziul Hoque <mikatiithost@gmail.com>
2024-11-22 02:03:36 +0600

    [done] fix cors error

--- src/app.js
+++ src/app.js
@@ -18,6 +18,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
