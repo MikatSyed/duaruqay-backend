6e6fe7368c8397963f2859304998fe410bf37e5c
Syed Gaziul Hoque <mikatiithost@gmail.com>
2024-11-22 00:36:46 +0600

    final

--- src/errors/ApiError.ts
+++ src/errors/ApiError.ts
@@ -1,11 +1,9 @@
 class ApiError extends Error {
   constructor(message: string, public statusCode: number) {
     super(message);
-
-    // Ensure the name of this error is the same as the class name
     this.name = this.constructor.name;
 
-    // Capture the stack trace for better debugging
+    // Ensure stack trace is available in V8-based engines
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
